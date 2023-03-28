c76656197afd7a03079cc9d9b874fe19b06aa122
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-28 14:10:27 -0400

    make todos and TodoItem compoent and have a data model class called Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Todos from './components/Todos';
+import Todo from './model/Todo';
 
 function App() {
+  const TodoList = [
+    new Todo("Learn React twice?"), 
+    new Todo("Learn Typescript")
+  ]
   return (
     <div>
-     <Todos/>
+      <Todos items={ TodoList} />
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,15 @@
-function Todos() {
+import React from "react";
+import Todo from "../model/Todo";
+import TodoItem from "./TodoItem";
+// const Todos:React.FC<{items:Todo[]}>= (props) => {
+//     return (<ul>
+//         {props.items.map(item => <li key={item.id}>{ item.text}</li>)}
+//     </ul>)
+// }
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+    console.log(props.items)
     return (<ul>
-        <li>Learn React</li>
-        <li>Learn Typescript</li>
+        {props.items.map(item => <TodoItem text={item.text} key={item.id} />)}
     </ul>)
 }
 
